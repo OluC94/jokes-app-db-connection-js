749571186ec6d19ffc59241a7b2440aa2556f76b
OluC94 <coker.olu94@gmail.com>
2024-07-17 13:35:42 +0100

    formatting linting and file cleanup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
 import { app } from "./support/setupExpress.js";
 import { query } from "./support/db.js";
-import { sum } from "./sum.js";
-import { setupARouteHandlerDemonstratingValidationWithZod } from "./zodDemo/setupARouteHandlerDemonstratingValidationWithZod.js";
 
 //You should delete all of these route handlers and replace them according to your own requirements
 
@@ -52,33 +50,6 @@ app.delete("/jokes/:id", async (req, res) => {
     }
     res.json({ outcome: "item successfully deleted" });
 });
-
-//just an example route handler.  delete it.
-app.get("/sum/:a/:b", handleGETRequestForSum);
-
-//This jsdoc comment helps vscode figure out the correct types for req and res for autocompletion, etc,
-//when it can't figure it out from context.
-/**
- * @param {import('express').Request} req
- * @param {import('express').Response} res
- */
-function handleGETRequestForSum(req, res) {
-    const answer = sum(parseInt(req.params.a), parseInt(req.params.b));
-    res.json({ answer });
-}
-
-//An example route that makes an SQL query to the db.
-app.get("/db-check", async (req, res) => {
-    try {
-        const dbResult = await query("select * from my_table");
-        res.json(dbResult.rows);
-    } catch (error) {
-        console.error("error handling db-check: ", error);
-    }
-});
-
-//Delete this, too.  It's just a demo for one way to robustly validate user-submitted data.
-setupARouteHandlerDemonstratingValidationWithZod(app);
 
 // use the environment variable PORT, or 4000 as a fallback
 const PORT = process.env.PORT ?? 4000;
